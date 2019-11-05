71ff3921f6086e1faed531221ae479dccc0abf06
radhsyn83 <radhsyn83@gmail.com>
2019-11-05 13:12:58 +0700

    first commit

--- src/com/radhsyn83/Main.java
+++ src/com/radhsyn83/Main.java
@@ -11,20 +11,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Android adr = new Android("Samsung","Android","J5",3000000);
+        Android adr = new Android("Samsung","Android","Grand",3000000);
         adr.setKeyStore("234ibfd3840fo");
         adr.displayProduct();
         System.out.println("Android KeyStore : "+adr.getKeyStore());
         System.out.println("");
 
         Blackberry bb = new Blackberry("Balckberry","RIM","Curve",2000000);
-        bb.setPinBB("BH167GT");
+        bb.setPinBB("BHS249");
         bb.displayProduct();
         System.out.println("PIN : "+bb.getPinBB());
         System.out.println("");
 
         WindowsPhone wp = new WindowsPhone("Nokia","Win8","Lumia",3500000);
-        wp.setWpKeyStore("S415BN");
+        wp.setWpKeyStore("UUQJWROJ");
         wp.displayProduct();
         System.out.println("Wp Key Store : "+wp.getWpKeyStore());
     }
